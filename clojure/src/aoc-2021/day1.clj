5c00e1a8fa0d83afb57bcd688ede85eaeb062695
Scott Bale <scott@balehaus.org>
2021-11-30 23:14:08 -0600

    2021 day 1

--- clojure/src/aoc-2021/day1.clj
+++ clojure/src/aoc-2021/day1.clj
@@ -1,5 +1,6 @@
 (ns aoc-2021.day1
   (:require
+   [clojure.edn :as edn]
    [clojure.java.io :as io]
    [debugger :refer [dbg]]))
 
@@ -7,17 +8,29 @@
 (defn runner
   "Run the puzzle with sequence of puzzle input, return the answer"
   [input]
-  )
+  (let [diffs (map - input (cons 0 input))]
+    (->> diffs 
+         rest
+         (filter pos?)
+         count)))
 
 (comment
 
   ;; Run with test input from the puzzle description
-  (runner ["puzzle test input line 1"
-           "puzzle test input line 2"])
+  (runner [199
+           200
+           208
+           210
+           200
+           207
+           240
+           269
+           260
+           263])
 
   ;; Run with the real input, get the final answer
   (with-open [r (io/reader (io/resource "aoc-2021/day1.txt"))]
-    (runner (line-seq r)))
+    (runner (map edn/read-string (line-seq r))))
 
 
   )
