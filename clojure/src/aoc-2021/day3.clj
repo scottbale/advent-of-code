94d3d45a9e9b96f72d5b01783d05cd6fd0d06117
Scott Bale <scott@balehaus.org>
2021-12-03 13:52:25 -0600

    2021 day 3 pt 2 ugh and I thought pt 1 was bad

--- clojure/src/aoc-2021/day3.clj
+++ clojure/src/aoc-2021/day3.clj
@@ -1,11 +1,9 @@
 (ns aoc-2021.day3
-  "Namespace docstring - don't forget to delete unused requires"
+  "Submarine status codes encoded in binary input"
   (:require
-   [aoc-2020.day2 :refer [nm]]
    [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [debugger :refer [dbg]]
-   [the.parsatron :as p]))
+   [debugger :refer [dbg]]))
 
 (defn read-binary
   "Read one of the e.g. '10010' input values as a binary literal (a long)"
@@ -21,8 +19,7 @@
         sum (reduce + numbers)]
     (cond
       (< sum n-half) 0
-      (> sum n-half) 1
-      :error (RuntimeException. "Neither bit is more frequent"))))
+      (>= sum n-half) 1)))
 
 (defn common-bit-at-bit
   "For the input seq and the bit-nm index, calculate the common bit and bitshift the result"
@@ -38,7 +35,7 @@
   [[1 0] [2 1] [4 2] [8 3] [16 4] [32 5]
    [64 6] [128 7] [256 8] [512 9] [1024 10] [2048 11]])
 
-(defn runner
+(defn runner-pt1
   "Runner docstring"
   [input]
   (let [n (count input)
@@ -49,21 +46,35 @@
         epsilon (bit-xor gamma xor-mask)]
     (* gamma epsilon)))
 
-(comment
+(defn sonic-recursor 
+  "Do the thing with teh stuff"
+  [in [[_ bit-position :as bit-thing] & more]]
+  (let [the-bit (common-bit-at-bit in bit-thing)
+        filter-fn (fn [i] (bit-test i bit-position))
+        in' (if (pos? the-bit)
+              (remove filter-fn in)
+              (filter filter-fn in))]
+    (if (== 1 (count in'))
+      (first in')
+      (recur in' more))))
 
-  (common-bit [1 0 1 0 0])
-  (common-bit [1 0 1 0 1 1])
-  (common-bit-at-bit [1 0 1 0 1 1] [1 0])
-  (common-bit-at-bit [2 0 3 0 2 2] [2 1])
-  (common-bit-at-bit [2 1 3 1 3 3] [2 1])
-  (common-bit-at-bit [16 1 3 1 3 3] [16 4])
-  (common-bit-at-bit [16 0 0] [16 4])
-  (common-bit-at-bit [16 15 15] [16 4])
-  (common-bit-at-bit [16 8 8 8 8 8] [16 4])
-  ;; wtf
-  (common-bit-at-bit '(4 30 22 23 21 15 7 28 16 25 2 10) [8 3])
-  (map (partial bit-and 8) '(4 30 22 23 21 15 7 28 16 25 2 10))
-  (common-bit '(0 8 0 0 0 8 0 8 0 8 0 8))
+(defn runner
+  "Runner docstring"
+  [input]
+  (let [n (count input)
+        width (count (first input))
+        codes (map read-binary input)
+        ;;
+        o2 (sonic-recursor codes (reverse (take width bit-thingies)))
+        ;;
+        xor-mask (edn/read-string (apply str "2r" (repeat width "1")))
+        co2 (bit-xor o2 xor-mask)]
+    (dbg [o2 co2])
+    ;;(* o2 co2)
+    o2
+    ))
+
+(comment
 
   ;; Run with test input from the puzzle description
   (runner ["00100"
@@ -84,13 +95,31 @@
            "010011101110"
            "110001001010"])
 
+  ;; Run with the real input, get the final answer
+  (with-open [r (io/reader (io/resource "aoc-2021/day3.txt"))]
+    (runner (line-seq r))) ;; pt 2 c02 257
+  ;; pt 2 o2 [3089 1006]        ;; pt 1 1025636
+  (* 3089 257) ;; 793873
+
+  (common-bit [1 0 1 0 0])
+  (common-bit [1 0 1 0 1 1])
+  (common-bit-at-bit [1 0 1 0 1 1] [1 0])
+  (common-bit-at-bit [2 0 3 0 2 2] [2 1])
+  (common-bit-at-bit [2 1 3 1 3 3] [2 1])
+  (common-bit-at-bit [16 1 3 1 3 3] [16 4])
+  (common-bit-at-bit [16 0 0] [16 4])
+  (common-bit-at-bit [16 15 15] [16 4])
+  (common-bit-at-bit [16 8 8 8 8 8] [16 4])
+  ;; wtf
+  (common-bit-at-bit '(4 30 22 23 21 15 7 28 16 25 2 10) [8 3])
+  (map (partial bit-and 8) '(4 30 22 23 21 15 7 28 16 25 2 10))
+  (common-bit '(0 8 0 0 0 8 0 8 0 8 0 8))
+
+
   (common-bit [1 0 1 0 1 1])
   (common-bit-at-bit [1 0 1 0 1 1] [1 0])
 
 
-  ;; Run with the real input, get the final answer
-  (with-open [r (io/reader (io/resource "aoc-2021/day3.txt"))]
-    (runner (line-seq r))) ;; 1025636
 
 
   )
