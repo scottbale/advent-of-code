f5da1553490309554b76315ff43d29e5377e1583
Scott Bale <scott@balehaus.org>
2021-12-24 10:25:47 -0600

    2021 day 12 pt 2 - clojure

--- clojure/src/aoc-2021/day12.clj
+++ clojure/src/aoc-2021/day12.clj
@@ -27,30 +27,53 @@
            (maybe-update n2 n1)))
      {} node-pairs)))
 
+(defn complete? [partial-route]
+  (= "end" (last partial-route)))
+
+(defn upper? [n]
+  (Character/isUpperCase (first n)))
+
+(defn keep?
+  "only keep candidate new route if candidate-node is uppercase or not already in the route"
+  [partial-route candidate-node]
+  (or (upper? candidate-node) (nil? (some #{candidate-node} partial-route))))
+
+(defn permit-small-cave-twice?
+  "Return true if no small cave has yet been visited twice"
+  [partial-route]
+  (->> (frequencies partial-route)
+       (filter (fn [[k v]] (and (not (upper? k)) (== 2 v))))
+       count
+       zero?))
+
+(defn keep-pt2?
+  "Like `keep?`, but allow routes which visit one small cave twice"
+  [partial-route candidate-node]
+  (or
+   (upper? candidate-node)
+   (nil? (some #{candidate-node} partial-route))
+   (permit-small-cave-twice? partial-route)))
+
 (defn find-routes
-  "Given the adjacency map, recursively find and return the list of all routes from 'start' to 'end'
-  that don't visit a lowercase cave more than once."
-  [adjacency-map]
-  (letfn [(complete? [partial-route]
-            (= "end" (last partial-route)))
-          (upper? [n]
-            (Character/isUpperCase (first n)))
-          (keep? [partial-route candidate-node]
-            ;; only keep candidate new route if candidate-node is uppercase or not already in the route
-            (or (upper? candidate-node) (nil? (some #{candidate-node} partial-route))))
-          (route-goal [partial-route]
+  "Given the adjacency map, recursively find and return the list of all routes from 'start' to 'end'.
+  `keep-fn` accepts a partial route (seq of strings) and a candidate node (String) and decides
+  whether the new resulting route is valid or not. "
+  [keep-fn adjacency-map]
+  (letfn [(route-goal [partial-route]
             ;; given partial-route, return coll of one or more longer routes
             (let [n (last partial-route)
                   nodes (adjacency-map n)]
               (reduce (fn [keep n]
-                        (if (keep? partial-route n)
+                        (if (keep-fn partial-route n)
                           (conj keep (conj partial-route n))
                           keep)) #{} nodes)))
           (step [complete-routes [r & more :as partial-routes]]
             (if r
               (if (complete? r)
                 (recur (conj complete-routes r) more)
-                (recur complete-routes (concat more (route-goal r))))
+                ;; `into` rather than `concat` - much faster and prevents stackoverflow in part 2
+                ;; thank you once again https://stuartsierra.com/2015/04/26/clojure-donts-concat
+                (recur complete-routes (into more (route-goal r))))
               complete-routes))]
     (step #{} #{["start"]})))
 
@@ -60,7 +83,17 @@
   (->> input
        (map parse-line)
        adjacency-map
-       find-routes
+       (find-routes keep?)
+       count))
+
+(defn runner-pt2
+  "Like part 1, except additionally include routes where one small cave can be visited twice per
+  route."
+  [input]
+  (->> input
+       (map parse-line)
+       adjacency-map
+       (find-routes keep-pt2?)
        count))
 
 
@@ -81,8 +114,22 @@
   ;;     \   /
   ;;      end
 
-  (with-open [r (io/reader (io/resource "aoc-2021/day12.txt"))]
-    (runner-pt1 (line-seq r))) ;; 3298
+  (time (with-open [r (io/reader (io/resource "aoc-2021/day12.txt"))]
+          (runner-pt1 (line-seq r)))) ;; 3298 "Elapsed time: 192.857966 msecs"
+
+
+  ;; pt 2
+
+  (runner-pt2 ["start-A"
+               "start-b"
+               "A-c"
+               "A-b"
+               "b-d"
+               "A-end"
+               "b-end"]) ;; 36
+
+  (time (with-open [r (io/reader (io/resource "aoc-2021/day12.txt"))]
+          (runner-pt2 (line-seq r)))) ;; 93572 "Elapsed time: 27915.674023 msecs"
 
 
   (parse-line "start-A")
@@ -90,7 +137,13 @@
 
   (find-routes {"start" '("b" "A"), "A" '("end" "b" "c"), "c" '("A"), "b" '("end" "d" "A"), "d" '("b")})
 
+  #{["start" "A" "c" "A" "b" "end"] ["start" "A" "c" "A" "b" "A" "end"] ["start" "b" "A" "end"] ["start" "b" "A" "c" "A" "end"] ["start" "A" "end"] ["start" "b" "end"] ["start" "A" "b" "end"] ["start" "A" "b" "A" "c" "A" "end"] ["start" "A" "b" "A" "end"] ["start" "A" "c" "A" "end"]}
+
   (some #{4} [1 2 3])
 
+  (permit-small-cave-twice? ["start" "A" "c" "A" "b" "end"])
+
+  (->> {"start" 1, "A" 2, "c" 2, "b" 1, "end" 1}
+       (filter (fn [[k v]] (and (not (upper? k)) (== 2 v)))))
 
   )
