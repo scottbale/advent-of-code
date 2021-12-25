6a107d4fa6d04da8b87fe292b6e0e32551b2d88f
Scott Bale <scott@balehaus.org>
2021-12-24 22:58:55 -0600

    2021 day 13 pt 2 - clojure

--- clojure/src/aoc-2021/day13.clj
+++ clojure/src/aoc-2021/day13.clj
@@ -36,10 +36,59 @@
         folds (map parse-fold folds)]
     (count (set (map (partial fold-a-point (first folds)) points)))))
 
+(defn pretty-print
+  "Pretty-print the final grid of points, after all folds have been applied"
+  [folds points]
+  (let [max-x (second (last (filter (comp zero? first) folds)))
+        max-y (second (last (filter (comp pos? first) folds)))
+        grid (vec (repeat max-y (vec (repeat max-x \.))))
+        grid-fn (fn [grid [x y]]
+                  (update grid y #(assoc % x \#)))
+        grid (reduce grid-fn grid points)]
+    (println (s/join "\n" (map #(apply str %) grid)))))
+
+(defn runner-pt2
+  "Apply all the folds to the input set of points. What 8 letters do the points spell out?"
+  [input]
+  (let [[points _ folds] (partition-by empty? input)
+        points (set (map parse-point points))
+        folds (map parse-fold folds)]
+    (pretty-print
+     folds
+     (reduce
+      (fn [pts fold]
+        (set (map (partial fold-a-point fold) pts)))
+      points folds))))
 
 
 (comment
 
+  ;; pt 2
+
+  (runner-pt2 ["6,10"
+               "0,14"
+               "9,10"
+               "0,3"
+               "10,4"
+               "4,11"
+               "6,0"
+               "6,12"
+               "4,1"
+               "0,13"
+               "10,12"
+               "3,4"
+               "3,0"
+               "8,4"
+               "1,10"
+               "2,14"
+               "8,10"
+               "9,0"
+               ""
+               "fold along y=7"
+               "fold along x=5"])
+
+  (with-open [r (io/reader (io/resource "aoc-2021/day13.txt"))]
+    (runner-pt2 (line-seq r))) ;; PERCGJPB
 
   ;; pt 1
 
