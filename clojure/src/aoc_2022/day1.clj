2b3fc34133759908cb17c5b4e580d1a4be021d5d
Scott Bale <scott@balehaus.org>
2022-12-04 20:42:39 -0600

    2022 day 1

---
+++ clojure/src/aoc_2022/day1.clj
@@ -0,0 +1,47 @@
+(ns aoc-2022.day1
+  "Elf calories carried"
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [debugger :refer [dbg]]))
+
+(defn total
+  [strs]
+  (->> strs
+       (map #(Integer/parseInt %))
+       (reduce +)))
+
+(defn runner
+  "Each group of numbers (groups separated by blank line) are calories carried by elves. What is the
+  largest total?"
+  [input]
+  (->> input
+       (partition-by str/blank?)
+       (remove (comp str/blank? first))
+       (map total)
+       (sort >)
+       first))
+
+
+(comment
+
+  (runner ["1000"
+           "2000"
+           "3000"
+           ""
+           "4000"
+           ""
+           "5000"
+           "6000"
+           ""
+           "7000"
+           "8000"
+           "9000"
+           ""
+           "10000"]) ;; 24000
+
+  (with-open [r (io/reader (io/resource "aoc-2022/day1.txt"))]
+    (runner (line-seq r))) ;; 68467
+
+
+  )
