ef428831a2dc2076132b12bd992a7bc0ff275009
Scott Bale <scott@balehaus.org>
2022-12-05 20:07:02 -0600

    2022 day 3 pt 2

--- clojure/src/aoc_2022/day3.clj
+++ clojure/src/aoc_2022/day3.clj
@@ -20,13 +20,22 @@
         item (first (set/intersection s1 s2))]
     (priorities item)))
 
-(defn runner
+(defn runner1
   "Each line of input is a string representing the contents of two equally-sized partitions. The
   partitions are supposed to have unique items, but each input pair of partitions contains exactly
   one duplicate char."
   [input]
   (reduce + (map priority input)))
 
+(defn runner
+  "Every three lines of input is an elf group. Exactly one item (char) will be found in all three
+  rucksacks. Find the sum of the priorities of each of those items."
+  [input]
+  (let [elf-groups (partition 3 input)]
+    (letfn [(find-item [ps]
+              (let [ss (map set ps)]
+                (->> ss (apply set/intersection) first)))]
+      (reduce + (map (comp priorities find-item) elf-groups)))))
 
 (comment
 
@@ -35,10 +44,10 @@
            "PmmdzqPrVvPwwTWBwg"
            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
            "ttgJtRGJQctTZtZT"
-           "CrZsJsPPZsGzwwsLwLmpwMDw"]) ;; 157
+           "CrZsJsPPZsGzwwsLwLmpwMDw"]) ;; 70 ;; 157
 
   (with-open [r (io/reader (io/resource "aoc-2022/day3.txt"))]
-    (runner (line-seq r))) ;; 7863
+    (runner (line-seq r))) ;; 2488 ;; 7863
 
   (priority "vJrwpWtwJgWrhcsFMMfFFhFp")
 
