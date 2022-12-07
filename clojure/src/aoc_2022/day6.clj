c8742cfb88aef0ac98c65530f8208bd272f2fe8d
Scott Bale <scott@balehaus.org>
2022-12-06 20:50:57 -0600

    2022 day 6 pt 2

--- clojure/src/aoc_2022/day6.clj
+++ clojure/src/aoc_2022/day6.clj
@@ -4,40 +4,49 @@
    [clojure.java.io :as io]))
 
 (defn find-marker
-  "Recursively traverse input, with a FIFO queue of most recent 4 chars. At each step: if queue has
-  four unique chars then success, return `n`. Otherwise, recurse, incrementing `n`, popping next val
+  "Recursively traverse input, with a FIFO queue of most recent `n` chars. At each step: if queue has
+  `n` unique chars then success, return `i`. Otherwise, recurse, incrementing `i`, popping next val
   off input and into `fifo`, and dropping oldest `fifo` val."
-  [n fifo input]
-  (if (= 4 (count (set fifo)))
-    n
-    (recur (inc n) (-> fifo pop (conj (first input))) (rest input))))
+  [n i fifo input]
+  (if (= n (count (set fifo)))
+    i
+    (recur n (inc i) (-> fifo pop (conj (first input))) (rest input))))
 
 (defn runner
-  "Initialize a FIFO queue from the first four input vals. Starting with that, recursively search for
-  the start-of-packet marker."
-  [input]
+  "Initialize a FIFO queue from the first `n` input vals. Starting with that, recursively search for
+  the packet marker."
+  [n input]
   (let [input (first input)
-        n 4
         input' (drop n input)
         fifo (apply conj clojure.lang.PersistentQueue/EMPTY (take n input))]
-     (find-marker n fifo input')))
+     (find-marker n n fifo input')))
 
 
 (comment
 
-  (runner ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]) ;; 7
-  (runner ["bvwbjplbgvbhsrlpgdmjqwftvncz"]) ;; 5
-  (runner ["nppdvjthqldpwncqszvftbrmjlhg"]) ;; 6
-  (runner ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"]) ;; 10
+  ;; pt 1
+  (runner 4 ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]) ;; 7
+  (runner 4 ["bvwbjplbgvbhsrlpgdmjqwftvncz"]) ;; 5
+  (runner 4 ["nppdvjthqldpwncqszvftbrmjlhg"]) ;; 6
+  (runner 4 ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"]) ;; 10
 
   (with-open [r (io/reader (io/resource "aoc-2022/day6.txt"))]
-    (runner (line-seq r))) ;; 1134
+    (runner 4 (line-seq r))) ;; 1134
+
+  ;; pt 2
+  (runner 14 ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]) ;; 19
+  (runner 14 ["bvwbjplbgvbhsrlpgdmjqwftvncz"]) ;; 23
+  (runner 14 ["nppdvjthqldpwncqszvftbrmjlhg"]) ;; 23
+  (runner 14 ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"]) ;; 29
+
+  (with-open [r (io/reader (io/resource "aoc-2022/day6.txt"))]
+    (runner 14 (line-seq r))) ;; 2263
 
   ;; expect 12?
   (let [fifo clojure.lang.PersistentQueue/EMPTY
         fifo (conj fifo \a \b \a \c)
         input "abaadabcfoobar"]
-    (find-marker 4 fifo input)) 
+    (find-marker 4 4 fifo input))
 
   (into (list) (conj (pop (conj clojure.lang.PersistentQueue/EMPTY 1 2 3)) 4 5))
   (set (conj (pop (conj clojure.lang.PersistentQueue/EMPTY 1 2 3)) 4 5))
