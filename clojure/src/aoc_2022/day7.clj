af17d6571e862ff63d42af1ca60db25de2303b2c
Scott Bale <scott@balehaus.org>
2022-12-21 12:24:26 -0600

    2022 day 7 pt 2

--- clojure/src/aoc_2022/day7.clj
+++ clojure/src/aoc_2022/day7.clj
@@ -4,9 +4,7 @@
    [clojure.edn :as edn]
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [clojure.zip :as z]
-   ;; [debugger :refer [dbg]]
-   ))
+   [clojure.zip :as z]))
 
 (defn z-total
   "Given a zipper data structure like [n [i ...] [j ...] ...]], return an updated zipper where i,j,...
@@ -28,11 +26,11 @@
 (defn z-dir
   "append an empty dir at `loc`"
   [loc]
-  (-> loc
-      (z/append-child [0])))
+  (z/append-child loc [0]))
 
 (defn z-step
-  "reduce fn, takes zipper and current instruction, returns possibly updated zipper"
+  "reduce fn, takes zipper and current instruction, returns possibly updated zipper. Uses `reduced` to
+  indicate completion, because the implementation reduces over an infinite sequence of instructions."
   [loc inst]
   (let [[a b c] (str/split inst #" ")]
     (cond
@@ -42,22 +40,65 @@
       ;; detect a cd, either into a subdir or up
       (and (= "$" a) (= "cd" b))
       (if (= ".." c)
-        (-> loc z-total z/up)
+        ;; Already at the root of the tree?
+        (if (nil? (z/up loc))
+          ;; Finished, short-circuit reduction
+          (reduced (z-total loc))
+          ;; Else calculate dir total and navigate upward one level
+          (-> loc z-total z/up))
+        ;; Else append a new empty subdir and navigate down to it
         (-> loc z-dir z/down z/rightmost))
       :else
       loc)))
 
-(defn runner
-  "runner docstring"
+(defn z-runner
+  "Given the input sequence, return a zipper structure that represents the filesystem's directory
+  hierarchy and sizes. It will be a vector of longs and nested vectors.
+
+  For example: `[117 [10 [5 2 3]] [7]]`
+
+  The invariant is that the first element of every vector should be a simple number which is the
+  total size of that (sub)directory. The size includes size(s) of file(s) in that dir (which are not
+  otherwise represented) and size(s) of any nested subdir(s). In this example, the root dir has a
+  size of 117, which is the sum of subdirs sizes (10 + 7) plus another 100 of files.
+
+  One tricky bit is that the puzzle input does not necessarily `cd ..` back to the root of the
+  filesystem tree. This implementation uses the `cd ..` to prompt calculation of the total of each
+  directory, so it's necessary to conclude the traversal by `cd ..` back to the root. I've done that
+  by concating a lazy infinite sequence of `$ cd ..` on to the end of the puzzle. But this means
+  that the `reduce` has to be written to short-circuit, using `reduced`."
+  [input]
+  (->> (concat input (repeat "$ cd .."))
+       rest ;; ignore the first instruction `$ cd /`
+       (reduce z-step (z/vector-zip [0]))))
+
+(defn runner1
+  "Pt 1: find the sum of sizes of all subdirectories not more than 100k in size."
   [input]
   (->> input
-       rest ;; ignore the first instruction `$ cd /`
-       (reduce z-step (z/vector-zip [0]))
-       (z/root)
-       (tree-seq vector? seq)    
+       z-runner
+       z/root
+       (tree-seq vector? seq)
        (filter number?)
        (filter #(>= 100000 %))
        (reduce +)))
+
+(defn runner
+  "Pt 2: find size of the smallest directory that, if deleted, will free up enough disk space."
+  [input]
+  (let [fs 70000000
+        needed 30000000
+        z-root (z-runner input)
+        root (z/root z-root)
+        dirs (->> root
+                  (tree-seq vector? seq)
+                  (filter number?))
+        used (first dirs)
+        available (- fs used)
+        target (- needed available)
+        pred (fn [x] (when (<= target x) x))
+        sorted-low-to-high (sort dirs)]
+    (some pred sorted-low-to-high)))
 
 
 (comment
@@ -84,10 +125,11 @@
            "4060174 j"
            "8033020 d.log"
            "5626152 d.ext"
-           "7214296 k"]) ;; 95437
+           "7214296 k"]) ;; 24933642 ;; 95437
+  ;; full tree should be [48381165 [94853 [584]] [24933642]]
 
   (with-open [r (io/reader (io/resource "aoc-2022/day7.txt"))]
-    (runner (line-seq r))) ;; 1555642
+    (runner (line-seq r))) ;; 5974547 ;; 15584 too low ;; 1555642
 
   (->> [48381165
         #_a [94853
@@ -116,6 +158,15 @@
   (-> (z/vector-zip [0 [1] [14]])
       (z-add 3)
       (z-add 7)
+      z/down
+      z/up
+      z/up
+      ;;z/root
+      )
+
+  (-> (z/vector-zip [0 [1] [14]])
+      (z-add 3)
+      (z-add 7)
       z/root)
 
   (-> (z/vector-zip [0 [1] [14]])
@@ -135,30 +186,30 @@
                 (let [loc (z/down rs)]
                   (z/up (z/edit loc + (map first (z/rights loc))))))
         x (-> vz
-            (z/down)
-            (z/edit + 23)
-            (z/edit + 13)
-            (z/up)
-            (z/append-child [0])
-            (z/down)
-            (z/rightmost)
-            (z/down)
-            (z/edit + 10)
-            (z/up)
-            (z/up)
-            (z/append-child [0])
-            (z/down)
-            (z/rightmost)
-            (z/down)
-            (z/edit + 8)
-            (z/up)
-            (z/up)
-            (z/down)
-        ;; (z/rights)
-        ;; (z/edit + [10 8])
-        ;; (total)
-        ;; (z/root)
-            )]
+              (z/down)
+              (z/edit + 23)
+              (z/edit + 13)
+              (z/up)
+              (z/append-child [0])
+              (z/down)
+              (z/rightmost)
+              (z/down)
+              (z/edit + 10)
+              (z/up)
+              (z/up)
+              (z/append-child [0])
+              (z/down)
+              (z/rightmost)
+              (z/down)
+              (z/edit + 8)
+              (z/up)
+              (z/up)
+              (z/down)
+              ;; (z/rights)
+              ;; (z/edit + [10 8])
+              ;; (total)
+              ;; (z/root)
+              )]
     (z/root (apply z/edit x + [10 8])))
 
   ;; invariant: new node representing dir is [0] - zero initial size, zero subdirs
@@ -193,7 +244,7 @@
         ;; (z/root)
         ))
 
-  (let [;; vz (z/zipper vector? seq (fn [_ c] c) [[3 4]])
+  (let [ ;; vz (z/zipper vector? seq (fn [_ c] c) [[3 4]])
         vz (z/vector-zip [1 2 [3 4]])]
     (-> vz
 
@@ -201,8 +252,6 @@
         (z/right)
         ;; (z/prev)
         ))
-  
-
   (->> [13 [8 [5] [3]] [5]]
        (tree-seq vector? seq)
        (filter number?)
