0ecfcb659e1b332a1d16a9c7eecd951f0d7c32ff
Scott Bale <scott@balehaus.org>
2023-11-30 18:52:21 -0600

    doinking around with transducers in user namespace, probably doesn't belong in this repo but whatevs lol smh

--- clojure/src/user.clj
+++ clojure/src/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.string :as s]))
 
 (defn newday
   "Generate a new namespace and data resource file for the next day's puzzle.
@@ -18,3 +19,108 @@
     (io/make-parents resource-file)
     (spit resource-file "")
     (format "Created '%s' and '%s'" src-file resource-file)))
+
+
+(comment ;; fun with transducers
+
+  (def input
+    ["$ cd /"
+     "$ ls"
+     "dir a"
+     "14848514 b.txt"
+     "8504156 c.dat"
+     "dir d"
+     "$ cd a"
+     "$ ls"
+     "dir e"
+     "29116 f"
+     "2557 g"
+     "62596 h.lst"
+     "$ cd e"
+     "$ ls"
+     "584 i"
+     "$ cd .."
+     "$ cd .."
+     "$ cd d"
+     "$ ls"
+     "4060174 j"
+     "8033020 d.log"
+     "5626152 d.ext"
+     "7214296 k"])
+
+  (def xform-split-instruction
+    (fn [f]
+      (fn
+        ([] (f))
+        ([m] m)
+        ([m x]
+         (f m (s/split x #" "))))))
+
+  (def xform-split-instruction
+    (map #(s/split % #" ")))
+
+  (def xform-count
+    (fn [f]
+      (completing
+       (fn [m x]
+         (f m (count x))))))
+
+  (def xform-count
+    (map count))
+
+  (def xform-filter-shell-commands
+    (fn [f]
+      (completing
+       (fn [m [a :as x]]
+         (if (= "$" a)
+           (f m x)
+           m)))))
+
+  (def xform-filter-shell-commands
+    (filter (comp (partial = "$") first)))
+
+  (def xform-extract-command
+    (fn [f]
+      (completing
+       (fn [m [_ b]]
+         (f m b)))))
+
+  (def xform-extract-command
+    (map second))
+
+  (reduce str input)
+
+  (reduce #(+ %1 (count %2)) 0 input)
+  ;; (transduce xform2 str input)
+  ;; (transduce xform2 (completing #(+ %1 (count %2))) 0 input)
+
+  ;; count total number of input instruction pieces
+  (transduce (comp xform-split-instruction xform-count) + input)
+
+  ;; count total number of shell command input instruction pieces
+  (transduce (comp xform-split-instruction xform-filter-shell-commands xform-count) + input)
+
+  ;; see only the shell command instructions
+  (sequence (comp xform-split-instruction xform-filter-shell-commands xform-extract-command) input)
+
+  ;; see only the shell command instructions (non-transducer)
+  (->> input
+       (map #(s/split % #" "))
+       (filter (comp (partial = "$") first))
+       (map second))
+
+  ;; transducers again
+  (->> input
+       (sequence
+        (comp
+         (map #(s/split % #" "))
+         (filter (comp (partial = "$") first))
+         (map second))))
+
+  ;; test `transduce` that "if coll contains no items, return init and f is not called"
+  (transduce (map name) (fn
+                          ([m] (println "called") nil)
+                          ([m _] (println "calledd") m)) 0 [])
+
+  ;;end comment
+  )
