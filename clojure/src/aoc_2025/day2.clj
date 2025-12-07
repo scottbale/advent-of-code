ce98e0cdfd4d07608c305780c6d105abb297d3dd
Scott Bale <scott.bale@nubank.com.br>
2025-12-06 18:35:44 -0600

    2025 day 2 part 2

--- clojure/src/aoc_2025/day2.clj
+++ clojure/src/aoc_2025/day2.clj
@@ -3,8 +3,7 @@
   (:require
    [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [clojure.string :as s]
-   [debugger :refer [dbg]]))
+   [clojure.string :as s]))
 
 (defn invalid-id-str?
   "An id is invalid if it is composed of a single number repeated twice. So, an
@@ -33,16 +32,51 @@
         b (edn/read-string b)]
     (range a (inc b))))
 
-(defn runner
-  "runner docstring"
-  [input]
+(def invalid-N-id?
+  "Returns a function that checks whether an id has a pattern repeated N times? A
+  generalization of `invalid-id?` from 2 to N."
+  (memoize
+   (fn [n]
+     (fn [id]
+       (let [id-str (str id)
+             c (count id-str)]
+         (when (= 0 (mod c n))
+           (let [l (/ c n)]
+             (apply = (partition l id-str)))))))))
+
+(defn invalid-id-2?
+  "Part 2: an id is invalid if is composed of any number repeated _at least_
+  twice. Takes a number input."
+  [id]
+  (let [c (count (str id))]
+    (when (<= 2 c)
+      (let [ns (range 2 (inc c))
+            fns (map invalid-N-id? ns)]
+        ((apply some-fn fns) id)))))
+
+(defn runner-helper
+  [pred input]
   (let [id-ranges (s/split input #"\,")
         ids (mapcat id-range-str->range id-ranges)
-        invalid-ids (filter invalid-id? ids)]
+        invalid-ids (filter pred ids)]
     (reduce + invalid-ids)))
+
+(defn runner [input]
+  (runner-helper invalid-id? input))
+
+(defn runner2 [input]
+  (runner-helper invalid-id-2? input))
 
 
 (comment
+
+  ((invalid-N-id? 3) 212121)
+  ((invalid-N-id? 3) 5150)
+  ((invalid-N-id? 3) 212112)
+  (invalid-id-2? 5150)
+  (invalid-id-2? 5151)
+  (invalid-id-2? 1111111)
+  (invalid-id-2? 1111311)
 
   (invalid-id-str? "2112")
 
@@ -50,7 +84,10 @@
   
   (runner "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124") ;; 1227775554
 
+  (runner2 "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124") ;; 4174379265
+
   (runner (slurp (io/resource "aoc-2025/day2.txt"))) ;; 18595663903
+  (runner2 (slurp (io/resource "aoc-2025/day2.txt")));; 19058204438
 
   ;; end comment
   )
