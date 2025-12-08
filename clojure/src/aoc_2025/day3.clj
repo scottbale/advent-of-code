d3456185935b62976dd7a456a1f086fb3669ac80
Scott Bale <scott.bale@nubank.com.br>
2025-12-07 21:48:30 -0600

    2025 day 3 part 2

--- clojure/src/aoc_2025/day3.clj
+++ clojure/src/aoc_2025/day3.clj
@@ -10,26 +10,28 @@
 
 (defn max-n-digits
   "Given a sequence of 1-digit numbers, find the largest `n`-digit number that can
-  be assembled from in-order input digits."
+  be assembled from in-order input digits.
+
+  Recursive solution to build a result one digit at a time, in decreasing order of
+  magnitude:
+  * Find the max among the `search-digits` (but don't search the trailing (n-1)
+    `excluded-digits`, reserved for the next digit(s) search)
+  * shrink `excluded-digits` (termination condition)
+  * Accumulate `result-digits`
+  * At the next recur: only search the `search-digits` after the last max _but_
+    append the next previously excluded digit"
   [n digits]
   (->>
    (loop [search-digits (drop-last (dec n) digits)
-          next-digits (take-last (dec n) digits)
-          result-digits []
-          i 0]
-     ;; (println "---------------------------iteration" i)
+          excluded-digits (take-last (dec n) digits)
+          result-digits []]
      (let [mx (apply max search-digits)
            result-digits (conj result-digits mx)]
-       ;; (println "search digits:" search-digits)
-       ;; (println "next digits:" next-digits)
-       ;; (println "max:" mx)
-       ;; (println "results:" result-digits)
-       (if (seq next-digits)
+       (if (seq excluded-digits)
          (recur
-          (conj (->> search-digits (drop-while #(not= % mx)) rest vec) (first next-digits))
-          (rest next-digits)
-          result-digits
-          (inc i))
+          (conj (->> search-digits (drop-while #(not= % mx)) rest vec) (first excluded-digits))
+          (rest excluded-digits)
+          result-digits)
          result-digits)))
    (apply str)
    edn/read-string))
@@ -39,6 +41,13 @@
   [input]
   (->> input
        (map (comp (partial max-n-digits 2) nm-str->digits-seq))
+       (reduce + 0)))
+
+(defn runner2
+  "runner docstring"
+  [input]
+  (->> input
+       (map (comp (partial max-n-digits 12) nm-str->digits-seq))
        (reduce + 0)))
 
 
@@ -56,10 +65,18 @@
   (runner ["987654321111111"
            "811111111111119"
            "234234234234278"
-           "818181911112111"])
+           "818181911112111"]) ;; 357
+
+  (runner2 ["987654321111111"
+           "811111111111119"
+           "234234234234278"
+           "818181911112111"]) ;; 3121910778619
 
   (with-open [r (io/reader (io/resource "aoc-2025/day3.txt"))]
     (runner (line-seq r))) ;;17281
+
+  (with-open [r (io/reader (io/resource "aoc-2025/day3.txt"))]
+    (runner2 (line-seq r))) ;; 171388730430281
 
   ;; end comment
   )
