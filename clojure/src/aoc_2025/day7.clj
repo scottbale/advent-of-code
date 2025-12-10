bcd9be382d1aa8cf66e544191df33a838ea64427
Scott Bale <scott.bale@nubank.com.br>
2025-12-10 15:58:54 -0600

    2025 day 7 part 2 naive

--- clojure/src/aoc_2025/day7.clj
+++ clojure/src/aoc_2025/day7.clj
@@ -20,6 +20,26 @@
      (assoc m :tachyons #{})
      tachyons)))
 
+(defn step2
+  "The part 2 step function differs from part 1: the input is (1) a _coll_ of
+  previous states (representing each of the 'timelines'), initially of size 1;
+  and (2) the same `splitters` as before which represents the next line of
+  input. The output is a _collection_ of next states. If any splits occurred,
+  the size of the output collection will be larger than the size of the
+  input (up to twice as large). The state is simplified from part 1: no need to
+  track `:split-count`, and each state now only has a single tachyon number
+  index, so the state is simply a single number. (Important to retain
+  duplicates, because duplicates represent different timelines that arrive at
+  the same index.)"
+  [states splitters]
+  (reduce
+   (fn [new-states timeline-index]
+     (if (splitters timeline-index)
+       (conj new-states (dec timeline-index) (inc timeline-index))
+       (conj new-states timeline-index)))
+   []
+   states))
+
 (defn input->indices
   "Given a string line of input and a set of chars, return a coll of indices of
   any of the chars in the input line."
@@ -44,7 +64,29 @@
       :split-count 0}
      ins)))
 
+(defn runner2
+  "Reduce over the lines of input. Calculate the indices of tachyons at each
+  successive line, and tally the total number of unique timelines (which is
+  equivalent to the size of the timeline index collection after the final step)"
+  [[in & ins :as _input]]
+  (let [indices-f (partial input->indices #{\S \| \^})]
+    (count (reduce
+      (fn [m k]
+        (step2 m (indices-f k)))
+      (vec (indices-f in))
+      ins))))
+
 (comment
+
+  (step2 [1 7]
+         #{})
+  (step2 [1 7]
+         #{2 4})
+  (step2 [1 7]
+         #{2 7 9})
+  (step2 [1 3 4 5 7 8 10 11 13]
+         #{1 3 5 7 9 13})
+
 
   (input->indices #{\S \| \^} "......S......")
   (input->indices #{\S \| \^} "...|..S......")
@@ -76,8 +118,28 @@
            ".^.^.^.^.^...^."
            "..............."]);; {:split-count 21, :tachyons #{0 4 6 12 2 11 14 10 8}}
 
+  (runner2 [".......S......."
+            "..............."
+            ".......^......."
+            "..............."
+            "......^.^......"
+            "..............."
+            ".....^.^.^....."
+            "..............."
+            "....^.^...^...."
+            "..............."
+            "...^.^...^.^..."
+            "..............."
+            "..^...^.....^.."
+            "..............."
+            ".^.^.^.^.^...^."
+            "..............."]) ;; 40
+
   (with-open [r (io/reader (io/resource "aoc-2025/day7.txt"))]
     (runner (line-seq r))) ;; {:split-count 1628, :tachyons #{0 70 62 74 110 86 20 72 58 60 101 102 135 88 46 4 106 54 92 137 104 48 50 116 99 113 32 40 129 91 117 108 56 22 90 36 118 100 131 122 44 6 28 134 64 51 25 34 12 2 66 23 127 82 76 97 68 112 138 14 45 78 132 26 140 16 120 38 126 124 30 96 10 18 52 114 67 42 80 94 8 84}}
+
+  (with-open [r (io/reader (io/resource "aoc-2025/day7.txt"))]
+    (runner2 (line-seq r)))
 
   ;; end comment
   )
