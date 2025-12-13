2d0c9f3cd8420d9ff10cfea85c0dcad918d1f4b2
Scott Bale <scott.bale@nubank.com.br>
2025-12-13 15:27:51 -0600

    2025 day 8 part 2 lame semi-manual way

--- clojure/src/aoc_2025/day8.clj
+++ clojure/src/aoc_2025/day8.clj
@@ -23,7 +23,7 @@
 
 (defn circuit-step
   "Reducing step function for collecting circuits. Takes a circuit map (keys are
-  points, values are sets of point triples) and a point triple (a vector triple
+  points, values are sets of points) and a point triple (a vector triple
   of distance, point1, point2) and returns a possibly-updated circuit map."
   [circuit-map [_d p1 p2 :as _triple]]
   (let [mega-circuit (conj (into (get circuit-map p1 #{})
@@ -46,17 +46,44 @@
         (for [[i pt1] points-alist
               [j pt2] points-alist
               :when (< i j)]
-          [(d pt1 pt2) pt1 pt2])]
+          [(d pt1 pt2) pt1 pt2])
+        initial-circuit-map (dbg (reduce (fn [m k] (assoc m k #{k})) {} points))]
     (->>
      pairs
      (sort-by first)
      (take N)
-     (reduce circuit-step {})
+     (reduce circuit-step initial-circuit-map)
      vals
      (map count)
      (apply sorted-set-by (fn [x y] (compare y x)))
      (take 3)
      (reduce * 1))))
+
+(defn runner2
+  "Like part 1 but take until there is only one large circuit, then return the product of the x
+  coordinates of the final pair. I'm going to leave the `N` param and just manually do a binary
+  search until I find the right `N`."
+  [N input]
+  (let [points (->> input (map parse-pt))
+        points-alist (->> points (map vector (range)))
+        pairs ;; and distances
+        (for [[i pt1] points-alist
+              [j pt2] points-alist
+              :when (< i j)]
+          [(d pt1 pt2) pt1 pt2])
+        initial-circuit-map (reduce (fn [m k] (assoc m k #{k})) {} points)
+        sorted-N (->>
+                  pairs
+                  (sort-by first)
+                  (take N))]
+    #_(->>
+     sorted-N
+     (reduce circuit-step initial-circuit-map)
+     vals
+     set
+     count)
+    (let [[_ [x1] [x2]] (last sorted-N)]
+      (* x1 x2))))
 
 (comment
 
@@ -85,9 +112,34 @@
               "984,92,344"
               "425,690,689"]) ;; 40
 
+  (runner2 29 ["162,817,812"
+               "57,618,57"
+               "906,360,560"
+               "592,479,940"
+               "352,342,300"
+               "466,668,158"
+               "542,29,236"
+               "431,825,988"
+               "739,650,466"
+               "52,470,668"
+               "216,146,977"
+               "819,987,18"
+               "117,168,530"
+               "805,96,715"
+               "346,949,466"
+               "970,615,88"
+               "941,993,340"
+               "862,61,35"
+               "984,92,344"
+               "425,690,689"])
+
    (time (with-open [r (io/reader (io/resource "aoc-2025/day8.txt"))]
       (runner 1000 (line-seq r)))) ;; 121770
    ;; 24 wrong, reverse sort order
+
+   (time (with-open [r (io/reader (io/resource "aoc-2025/day8.txt"))]
+      (runner2 5674 (line-seq r)))) ;; 7893123992
+
 
    ;; list comprehension
    (let [ns (range 5)]
