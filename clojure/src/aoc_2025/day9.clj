8e1c9142af3dfef9b1d109d9a57bc90586ad0330
Scott Bale <scott.bale@nubank.com.br>
2025-12-17 23:17:58 -0600

    2025 day 9 part 2 goldplate

--- clojure/src/aoc_2025/day9.clj
+++ clojure/src/aoc_2025/day9.clj
@@ -29,21 +29,6 @@
                y ys]
            [x y]))))
 
-;; This resulted in OOME
-#_(defn interior-points
-  "Given a pair of two points which are opposite corners of a rectangle, return a
-  collection of all _interior_ points of that rectangle. That is, points that
-  are not along an edge. Note that this may be an empty collection for
-  rectangles of width one or two."
-  [[[x1 y1] [x2 y2]]]
-  (let [[x-min x-max] (if (<= x1 x2) [x1 x2] [x2 x1])
-        [y-min y-max] (if (<= y1 y2) [y1 y2] [y2 y1])
-        xs (range (inc x-min) x-max)
-        ys (range (inc y-min) y-max)]
-    (set (for [x xs
-               y ys]
-           [x y]))))
-
 (defn inside?
   "Predicate: given a point pair that defines a rectangle (opposite corners) and a
   candidate point, return truthy/falsey if the point is inside the rectangle's
@@ -53,23 +38,24 @@
         [y-min y-max] (if (<= y1 y2) [y1 y2] [y2 y1])]
     (and (< x-min x x-max) (< y-min y y-max))))
 
+(defn runner*
+  [points pred]
+  (let [points-alist (mapv vector (range) points)
+        point-pairs (for [[i p1] points-alist
+                          [j p2] points-alist
+                          :when (< i j)]
+                      [p1 p2])]
+    (->> point-pairs
+         (remove pred)
+         (mapv area)
+         sort
+         last)))
+
 (defn runner
   "Return the area of the largest rectangle that can be formed by two opposite
   corners from the points of the puzzle input."
   [input]
-  (let [points-alist
-        (->> input
-             (mapv parse-point)
-             (mapv vector (range)))
-        point-pairs
-        (for [[i p1] points-alist
-              [j p2] points-alist
-              :when (< i j)]
-          [p1 p2])]
-    (->> point-pairs
-         (mapv area)
-         sort
-         last)))
+  (runner* (mapv parse-point input) (constantly false)))
 
 (defn runner2
   "Like part 1, but the largest rectangle has to reside entirely within the
@@ -82,26 +68,15 @@
         (->>
          perimeter-point-pairs
          (map area-points)
-         (reduce into))
-        points-alist (mapv vector (range) points)
-        point-pairs
-        (for [[i p1] points-alist
-              [j p2] points-alist
-              :when (< i j)]
-          [p1 p2])]
-    (->>
-     point-pairs
+         (reduce into))]
      ;; Key insight: given a set of all points of the perimeter of the polygon,
      ;; remove any candidate rectangle which has any perimeter point in its
      ;; interior (not along its edge).
      ;; (An earlier attempt OOME'd by attempting to do clojure.set/intersection
-     ;; on `(interior-points point-pair perimeter-points)`, since the area of
-     ;; the largest rectangles were on the order of 1 billion.)
-     (remove (fn [point-pair]
-               (some (partial inside? point-pair) perimeter-points)))
-     (mapv area)
-     sort
-     last)))
+     ;; on `(interior-points point-pair)` and `perimeter-points`, since the area
+     ;; of the largest rectangles were on the order of 1 billion.)
+    (runner* points (fn [point-pair]
+                      (some (partial inside? point-pair) perimeter-points)))))
 
 (comment
 
@@ -116,10 +91,6 @@
   (area-points [[7 1] [11 1]])
   (area-points [[11 1] [7 1]])
   (area-points [[2 5] [11 1]])
-
-  ;; (interior-points [[7 1] [11 1]])
-  ;; (interior-points [[11 1] [7 1]])
-  ;; (interior-points [[2 5] [11 1]])
 
   (inside? [[7 1] [11 1]] [7 2])
   (inside? [[2 5] [11 1]] [3 3])
