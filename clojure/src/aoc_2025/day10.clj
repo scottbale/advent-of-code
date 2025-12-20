f0c4f84eeca30fb7addf23abbebef64e3d6b6255
Scott Bale <scott.bale@nubank.com.br>
2025-12-19 20:14:52 -0600

    2025 day 10 part 1

--- clojure/src/aoc_2025/day10.clj
+++ clojure/src/aoc_2025/day10.clj
@@ -1,10 +1,16 @@
 (ns aoc-2025.day10
-  "Push buttons to light machines up."
+  "Push buttons to light machines up. Represent input tokens as bytes."
   (:require
    [clojure.edn :as edn]
    [clojure.java.io :as io]
+   [clojure.math.combinatorics :as combo]
    [clojure.string :as s]
    [debugger :refer [dbg]]))
+
+(defn strip
+  "Strip brackets bracketing input string token"
+  [token]
+  (subs token 1 (-> token count dec)))
 
 (defn parse-indicator-light-diagram
   "Parse the first thing in the input into a byte where the `#` pound signs are
@@ -12,9 +18,7 @@
   [input-str]
   (->>
    input-str
-   rest
-   reverse
-   rest
+   strip
    (mapv vector (range))
    (filter (comp (partial = \#) second))
    (mapv first)
@@ -24,24 +28,38 @@
   "Parse an e.g. `(0,2,3,4)` into a byte where each number is the index of a bit
   that will be set high."
   [input-str]
-  (let [input-str (subs input-str 1 (-> input-str count dec))
+  (let [input-str (strip input-str)
         nms (map edn/read-string (s/split input-str #","))]
     (reduce bit-set (byte 0x00) nms)))
 
 (defn process-one
-  "Process one line of input"
+  "Process one line of input. Each of the tokens in the line of input can be represented as a byte,
+  and it becomes a matter of (a) for increasingly larger combinations of the buttons, (b) apply
+  `bit-xor` to each combination to see if it yields the expected value from the first token. The
+  size of the combination is the 'number of button presses'."
   [input-str]
   (let [inputs (s/split input-str #" ")
         expected (parse-indicator-light-diagram (first inputs))
-        buttons (mapv parse-button-wiring-schematic (-> inputs rest drop-last))]
-    [expected buttons]
+        buttons (->>
+                 inputs
+                 rest
+                 drop-last
+                 (map parse-button-wiring-schematic))]
     (loop [i 1]
-      )))
+      (when (<= i (count buttons)) ;; safety valve, shouldn't be necessary
+        ;; https://github.com/clojure/math.combinatorics
+        (let [combos (combo/combinations buttons i)]
+          (if (some (fn [btns] (= expected (reduce bit-xor btns))) combos)
+            i
+            (recur (inc i))))))))
 
 (defn runner
-  "runner docstring"
+  "Sum the result (number of button presses) for each line of input."
   [inputs]
-  )
+  (->>
+   inputs
+   (map process-one)
+   (reduce + 0)))
 
 
 (comment
@@ -51,14 +69,17 @@
   (parse-indicator-light-diagram "[.###.#.]")
   (parse-button-wiring-schematic "(0,2,3,4)")
 
+  (bit-xor 0 (byte 0x02) (byte 0x04))
+
   (process-one "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
+  (process-one "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}")
 
   (runner ["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
-           "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"])
+           "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"]) ;; 7
 
   (with-open [r (io/reader (io/resource "aoc-2025/day10.txt"))]
-    (runner (line-seq r)))
+    (runner (line-seq r))) ;; 375
 
   ;; end comment
   )
