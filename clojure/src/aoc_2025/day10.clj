097f2991d495efb2ac65088abe9c98becaddb5a0
Scott Bale <scott.bale@nubank.com.br>
2025-12-22 08:34:12 -0600

    2025 day 10 WIP

--- clojure/src/aoc_2025/day10.clj
+++ clojure/src/aoc_2025/day10.clj
@@ -32,12 +32,19 @@
         nms (map edn/read-string (s/split input-str #","))]
     (reduce bit-set (byte 0x00) nms)))
 
-(defn parse-vector-of-nums
-  [token]
-  (mapv edn/read-string (->
-                         token
-                         strip
-                         (s/split #","))))
+(defn min-presses
+  "Given the sequence of buttons (each a byte), and a target (a byte), calculate the minimum number of
+  button presses that can achieve the target using some combination of button presses. Recursively,
+  for increasingly larger combinations of the buttons, apply `bit-xor` to each combination to see if
+  it yields the target byte value."
+  [buttons target]
+  (loop [i 1]
+    (when (<= i (count buttons)) ;; safety valve, shouldn't be necessary
+      ;; https://github.com/clojure/math.combinatorics
+      (let [combos (combo/combinations buttons i)]
+        (if (some (fn [btns] (= target (reduce bit-xor btns))) combos)
+          i
+          (recur (inc i)))))))
 
 (defn process-one
   "Process one line of input. Each of the tokens in the line of input can be represented as a byte,
@@ -52,51 +59,7 @@
                  rest
                  drop-last
                  (map parse-button-wiring-schematic))]
-    (loop [i 1]
-      (when (<= i (count buttons)) ;; safety valve, shouldn't be necessary
-        ;; https://github.com/clojure/math.combinatorics
-        (let [combos (combo/combinations buttons i)]
-          (if (some (fn [btns] (= expected (reduce bit-xor btns))) combos)
-            i
-            (recur (inc i))))))))
-
-(defn step
-  [expected buttons wips wip]
-  (let [r (reduce
-           (fn [wips button]
-             (let [wip' (reduce
-                         (fn [w i]
-                           (let [w (update w i inc)]
-                             (cond
-                               (= expected w) (reduced :match)              ;; found a match
-                               (< (get expected i) (get w i)) (reduced nil) ;; too high, don't add
-                               :else w)))
-                         wip button)]
-               (cond
-                 (= :match wip') (reduced :match) ;; found a match
-                 (nil? wip') wips                 ;; too high, don't add
-                 :else (conj wips wip'))))
-           wips buttons)]
-    (if (= :match r)
-      (reduced r)
-      r)))
-
-(defn process-one-pt2
-  [input-str]
-  (let [inputs (rest (s/split input-str #" "))
-        expected (parse-vector-of-nums (last inputs))
-        buttons (->>
-                 inputs
-                 drop-last
-                 (map parse-vector-of-nums))
-        init (vec (repeat (count expected) 0))]
-    (loop [i 1
-           wips (set (repeat (count buttons) init))]
-
-      (let [result (reduce (partial step expected buttons) #{} wips)]
-        (if (= :match result)
-          i ;; found the match
-          (recur (inc i) result))))))
+    (min-presses buttons expected)))
 
 (defn runner
   "Sum the result (number of button presses) for each line of input."
@@ -106,13 +69,6 @@
    (map process-one)
    (reduce + 0)))
 
-(defn runner2
-  [inputs]
-  (->>
-   inputs
-   (map process-one-pt2)
-   (reduce + 0)))
-
 
 (comment
 
@@ -120,32 +76,28 @@
   (parse-indicator-light-diagram "[...#.]")
   (parse-indicator-light-diagram "[.###.#.]")
   (parse-button-wiring-schematic "(0,2,3,4)")
-  (parse-vector-of-nums "(0,2,3,4)")
+  (parse-button-wiring-schematic "(3)") 
 
   (bit-xor 0 (byte 0x02) (byte 0x04))
 
   (process-one "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
   (process-one "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}")
-
-  (process-one-pt2 "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}")
-
-  (step [3 5 4 7] '([0 1] [2 3] [3]) #{[1 1 1 1]} [3 5 2 3] )
-  (step [3 5 4 7] '([0 1] [2 3] [3]) #{[1 1 1 1]} [3 5 3 6] )
-
+  (min-presses [(byte 0x08) (byte 0x10) (byte 0x04) (byte 0x12) (byte 0x05) (byte 0x03)] (byte 0x06))
+  (min-presses [(byte 0x08) (byte 0x10) (byte 0x04) (byte 0x12) (byte 0x05) (byte 0x03)] (byte 0x20)x)
 
   (runner ["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"]) ;; 7
 
-  (runner2 ["[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
-            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
-            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"]) ;; 33
-
   (with-open [r (io/reader (io/resource "aoc-2025/day10.txt"))]
     (runner (line-seq r))) ;; 375
 
-  (time (with-open [r (io/reader (io/resource "aoc-2025/day10.txt"))]
-     (runner2 (line-seq r))))
+  "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+  "P(3,5,4,7)"
+  "P(1,1,0,1) + P(2,4,4,6)"
+  "P(1,1,0,1) + 2*P(1,2,2,3)"
+  "P(1,1,0,1) + 2*[P(1,0,0,1) + 2*P(0,1,1,1)]"
+  
 
   ;; end comment
   )
